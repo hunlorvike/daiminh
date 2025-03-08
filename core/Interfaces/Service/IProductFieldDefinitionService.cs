60dc42764b8c45c36bc4fa7cad07bf3eb1f55167
ste114r <uehi.1270@gmail.com>
2025-03-08 22:44:38 +0700

    Added product field definition admin feature

---
+++ core/Interfaces/Service/IProductFieldDefinitionService.cs
@@ -0,0 +1,13 @@
+﻿using Core.Common.Models;
+using core.Entities;
+
+namespace core.Interfaces.Service;
+
+public interface IProductFieldDefinitionService
+{
+    Task<List<ProductFieldDefinition>> GetAllAsync();
+    Task<ProductFieldDefinition?> GetByIdAsync(int id);
+    Task<BaseResponse> AddAsync(ProductFieldDefinition model);
+    Task<BaseResponse> UpdateAsync(int id, ProductFieldDefinition model);
+    Task<BaseResponse> DeleteAsync(int id);
+}
