a20f3a05c327fa2833c6d8a62cbf9380a6e16c54
nguyenviethung <hungcutedethuongg@gmail.com>
2025-03-18 01:05:37 +0700

    install rich text summernote

---
+++ src/web/wwwroot/js/Views/Admin/Content/Content.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $(document).on('shown.bs.modal', '#create-content--modal', function (e) {
+        let options = {
+            lang: 'vi-VN',
+            placeholder: 'Nhập nội dung bài viết',
+            tabsize: 2,
+            height: 300,
+            minHeight: 200,
+            maxHeight: 500
+        };
+        $('#ContentBody').summernote(options);
+    });
+});
+
