87d8719210890df5c74bef1ac049da7fb0c477e1
nguyen viet hung <hungcutedethuongg@gmail.com>
2025-04-24 09:51:09 +0700

    update config wysiwyg.js

--- src/web/wwwroot/js/wysiwyg.js
+++ src/web/wwwroot/js/wysiwyg.js
@@ -3,30 +3,32 @@ $(function () {
 
     const contentCssFiles = [
         '/lib/tabler/core/dist/css/tabler.min.css',
-        ...(theme === 'dark'
-            ? ['dark']
-            : []),
+        ...(theme === 'dark' ? ['dark'] : []),
+        '/fonts/inter/inter.css'
     ];
 
     const options = {
         selector: '.wysiwyg',
-        plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste help wordcount',
-            'fontfamily'
+        height: 500,
+        language: 'vi',
+        language_url: '/js/vi.js',
+              plugins: [
+            'advlist anchor autolink autosave charmap code codesample',
+            'directionality emoticons fullscreen help image importcss insertdatetime',
+            'link lists media nonbreaking preview save searchreplace table template',
+            'visualblocks visualchars wordcount'
         ].join(' '),
         toolbar: [
-            'undo redo | formatselect |',
-            'fontselect fontsizeselect |',
-            'bold italic underline | alignleft aligncenter alignright |',
-            'bullist numlist outdent indent | removeformat | preview'
+            'undo redo | bold italic underline strikethrough |',
+            'alignleft aligncenter alignright alignjustify |',
+            'bullist numlist outdent indent | link image media |',
+            'code | forecolor backcolor |',
+            'emoticons charmap | removeformat | preview fullscreen'
         ].join(' '),
         skin: theme === 'dark' ? 'oxide-dark' : 'oxide',
         content_css: contentCssFiles,
+        content_style: "body { font-family: 'Inter', sans-serif; }",
     };
 
     hugerte.init(options);
 });
-
-// TODO: chưa cấu hình được font trong hugerte
