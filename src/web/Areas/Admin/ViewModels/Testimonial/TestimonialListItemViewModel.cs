9c44242c31ab493716b7c60c27d02f4e76d57939
Nguyễn Việt Hưng <hungcutedethuongg@gmail.com>
2025-04-30 02:35:46 +0700

    Enhance testimonial management forms with improved layout, added client title, and star rating functionality.

--- src/web/Areas/Admin/ViewModels/Testimonial/TestimonialListItemViewModel.cs
+++ src/web/Areas/Admin/ViewModels/Testimonial/TestimonialListItemViewModel.cs
@@ -4,6 +4,7 @@ public class TestimonialListItemViewModel
 {
     public int Id { get; set; }
     public string? ClientAvatar { get; set; }
+    public string? ClientTitle { get; set; }
     public string ClientName { get; set; } = string.Empty;
     public string? ClientCompany { get; set; }
     public int Rating { get; set; }
