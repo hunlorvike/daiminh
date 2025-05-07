23ed77d5f6250f2be3434e104be8d6f89491ed6e
Nguyễn Việt Hưng <hungcutedethuongg@gmail.com>
2025-05-07 15:18:01 +0700

    Refactor Contact page layout and improve footer content; add Tom-select scripts and styles

--- src/web/Areas/Client/Controllers/ContactController.cs
+++ src/web/Areas/Client/Controllers/ContactController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace web.Areas.Client.Controllers;
 
 [Area("Client")]
+[Route("lien-he")]
 public class ContactController : Controller
 {
     public IActionResult Index()
