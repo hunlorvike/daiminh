1c333a8fd43946845b57962fda8ffc84a31b6f45
Nguyễn Việt Hưng <hungcutedethuongg@gmail.com>
2025-05-14 23:17:33 +0700

    ui: ecommerce

---
+++ src/web/Areas/Client/Controllers/CartController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace web.Areas.Client.Controllers;
+
+[Area("Client")]
+public class CartController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Checkout()
+    {
+        return View();
+    }
+
+    public IActionResult Orderconfirmation()
+    {
+        return View();
+    }
+}
