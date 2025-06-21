bdfec433abb6e4acdc38587802cb1cc83279a515
Nguyễn Việt Hưng <hungcutedethuongg@gmail.com>
2025-06-21 23:52:57 +0700

    og

---
+++ src/application/Dtos/Shared/BaseDto.cs
@@ -0,0 +1,9 @@
+namespace application.Dtos.Shared;
+public class BaseDto
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public string? UpdatedBy { get; set; }
+}
